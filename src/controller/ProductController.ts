cb2b1be070d948e19973096cff49ca38581b0b20
Alessandro B Lemos Jr <ablemosjunior@gmail.com>
2024-05-17 11:59:55 -0300

    feat: update product function/router

--- src/controller/ProductController.ts
+++ src/controller/ProductController.ts
@@ -52,7 +52,7 @@ export default {
     }
   },
 
-  async update(req: Request, res: Response) {
+  async update(req: Request, res: Response): Promise<Response> {
     const { id } = req.params;
     const { name, description, image, price, quantity, userId } = req.body;
 
@@ -72,6 +72,20 @@ export default {
       return res.status(200).json(product);
     } catch (error) {
       return res.status(400).json({ error: 'Erro ao atualizar produto.' });
+    }
+  },
+
+  async delete(req: Request, res: Response) {
+    const { id } = req.params;
+
+    try {
+      await prisma.product.delete({
+        where: { id: Number(id) }
+      });
+
+      return res.status(204).send();
+    } catch (error) {
+      res.status(500).json({ error: 'Erro ao deletar produto.' });
     }
   }
 }
--- src/router.ts
+++ src/router.ts
@@ -11,5 +11,6 @@ router.post('/products', ProductController.create);
 router.get('/products', ProductController.getAll);
 router.get('/products/:id', ProductController.getById);
 router.put('/products/:id', ProductController.update);
+router.delete('/products/:id', ProductController.delete);
 
 export default router;
