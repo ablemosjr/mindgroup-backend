bd4117e8bbb999bea91ac0bfc0c371e73428521b
Alessandro B Lemos Jr <ablemosjunior@gmail.com>
2024-05-18 14:05:01 -0300

    fix: cors problem

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import router from './router';
+import cors from 'cors';
 
 const app = express();
 const PORT = 3000;
+
+// Configuração CORS - Acesso ALL
+app.use(cors({ origin: '*' }));
 
 app.use(express.json());
 app.use(router);
